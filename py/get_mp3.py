aaaae3fbfdd150f64974707b15264fb26d5480b0
Ron Ngai <rondsny@gmail.com>
2018-04-02 23:55:49 +0800

    Update get_mp3.py

--- py/get_mp3.py
+++ py/get_mp3.py
@@ -34,7 +34,7 @@ def save_mp3(url, count):
         file.write(r.content)
     return True
 
-def test(url):
+def down_a_page(url):
     soup = create_soup(url)
     list1 = soup.find(class_ = "album_soundlist").find_all('li')
     count = 1000
@@ -47,28 +47,31 @@ def test(url):
         mp3_url = get_mp3_url(url)
         save_mp3(mp3_url, count)
 
+
+def get_page_count(url):
+    soup = create_soup(url)
+    list1 = soup.find(class_ = 'pagingBar').find_all('a')
+    count = 0
+    for msg1 in list1:
+        count = count + 1 
+
+    if(count>1):
+        return count - 1
+    else:
+        return 1
+
+def test(url):
+    page_count = get_page_count(url)
+    for index in range(page_count):
+        print index
+
+
 url = "http://www.ximalaya.com/54715628/album/4790468/"
 url = "http://www.ximalaya.com/96295838/album/12642897/"
 url = "http://www.ximalaya.com/44255360/album/5021722/"
 url = "http://www.ximalaya.com/44255360/album/5021722?page=2"
 url = "http://www.ximalaya.com/29044178/album/11953680/"
 url = "http://www.ximalaya.com/44190264/album/5088879/"
-url = "http://www.ximalaya.com/44190264/album/5088879?page=2"
-url = "http://www.ximalaya.com/44190264/album/5088879?page=3"
+# url = "http://www.ximalaya.com/44190264/album/5088879?page=2"
+# url = "http://www.ximalaya.com/44190264/album/5088879?page=3"
 test(url)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
